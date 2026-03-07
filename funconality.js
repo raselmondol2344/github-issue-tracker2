3f8c5e9a8bd278c8d2e2c0df11e3f6c587c89cb3
raselmondol2344 <raselmondream245@gmail.com>
2026-03-07 18:58:29 +0600

    login page design complete

---
+++ funconality.js
@@ -0,0 +1,33 @@
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", function () {
+    //1 get username 
+    const userNameInput = document.getElementById("user-name")
+    const name =userNameInput.value;
+    console.log(name);
+
+    // 2 get password
+    const passwordInput = document.getElementById("user-password")
+    const password =passwordInput.value;
+    console.log(password);
+
+   // 3 condition 
+   if (name =="admin" && password == "admin123")
+   {
+    //3.1 true :: go to home page
+    alert("login success")
+    window.location.href = "./home_page.html";
+    
+   }
+   else{ 
+    // 3.2 false ::  alert () & return 
+    alert("login failed");
+    return;
+   }
+
+
+   
+
+  
+
+});
